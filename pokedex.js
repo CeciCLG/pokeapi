0a53e65d91424722730b237cd7dcbac645bafdda
Cecilia Christina <cecilia.lares@bootcamp-upgrade.com>
2024-05-17 09:22:32 +0200

    get information from api

--- pokedex.js
+++ pokedex.js
@@ -1,5 +1,18 @@
-let pokedexOl = document.querySelector(`#pokedex`);
+const pokedexOl = document.querySelector(`#pokedex`);
 
-function collect() {
-
+async function collectPokemon(numOfPokemon) {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+        const data1 = await response.json();
+        console.log(data1);
+        const linkUrl = data1.results[numOfPokemon].url;
+        console.log(linkUrl);
+        const responseUrl = await fetch(linkUrl);
+        const data2 = await responseUrl.json();
+        console.log(data2);
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+collectPokemon(0);
